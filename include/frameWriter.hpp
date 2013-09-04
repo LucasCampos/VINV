c734ea023a2fa7ae659df2855d4b31c8e966f826
Lucas Campos <lqcc@df.ufpe.br>
2013-09-04 07:45:17 -0300

    ImageWriter adapted to FrameWriter

--- include/frameWriter.hpp
+++ include/frameWriter.hpp
@@ -22,14 +22,10 @@
  * This file sets the interface to imageWriter, and creates two instances,
  * one to write eps files, and one to write png files. 
  *
- * The imageWriter is needed abstract the specific interfaces of several 
- * image writing libraries. Indeed, if one wants to extend the available
- * formats to say, jpeg, one must create a imageWriter version to jpeg.
- *
  * */
 
-#ifndef IMAGEWRITER_HPP
-#define IMAGEWRITER_HPP
+#ifndef FRAMEWRITER_HPP
+#define FRAMEWRITER_HPP
 
 #include <vector>
 #include <cmath>
@@ -39,19 +35,25 @@
 
 /*
  *
- * General interface. One cannot instantiate a ImageWriter
+ * General interface. One cannot instantiate a FrameWriter
  *
  * */
 
-struct ImageWriter {
+struct FrameWriter {
 
 	double left;
 	double right;
 	double up;
 	double down;
 
-	virtual double NormalizingFactor() const = 0;
-	virtual double Center() const =0;
+	virtual double NormalizingFactorX() const = 0;
+	virtual double CenterX() const =0;
+	virtual double NormalizingFactorY() const = 0;
+	virtual double CenterY() const =0;
+	
+	virtual void StartFrame()=0;
+	virtual void EndFrame()=0;
+
 	virtual void line(double xfrom, double yfrom, double xto, double yto, double width, int red, int green, int blue)=0;
 	virtual void multiline (const std::vector<double>& x, const std::vector<double>& y, double width, int red, int green, int blue)=0;
 
@@ -70,31 +72,5 @@ struct ImageWriter {
 	virtual void close()=0;
 };
 
-struct OGLWriter {
-
-	double left;
-	double right;
-	double up;
-	double down;
-
-	virtual double NormalizingFactor() const = 0;
-	virtual double Center() const =0;
-	virtual void line(double xfrom, double yfrom, double xto, double yto, double width, int red, int green, int blue)=0;
-	virtual void multiline (const std::vector<double>& x, const std::vector<double>& y, double width, int red, int green, int blue)=0;
-
-	virtual void triangle(double x1, double y1, double x2, double y2, double x3, double y3, int red, int green, int blue)=0;
-	virtual void filledTriangle(double x1, double y1, double x2, double y2, double x3, double y3, int red, int green, int blue)=0;
-
-	virtual void square(double xfrom, double yfrom, double xto, double yto, int red, int green, int blue)=0;
-	virtual void filledSquare(double xfrom, double yfrom, double xto, double yto, int red, int green, int blue)=0;
-
-	virtual void circle(double xcentre, double ycentre, double radius, int red, int green, int blue)=0;
-	virtual void filledCircle(double xcentre, double ycentre, double radius, int red, int green, int blue)=0;
-
-	virtual void writeText(double x, double y, std::string name, double fontSize, int red, int green, int blue) =0;
-
-	virtual void printname()=0;
-	virtual void close()=0;
-};
 
 #endif
